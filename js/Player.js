3aa17c7c290eab136fefcd5c744fb6f0de7736b0
TanishtheGamer <tanishrreddy@gmail.com>
2021-04-22 17:45:24 -0700

    Ranking

--- js/Player.js
+++ js/Player.js
@@ -3,6 +3,7 @@ class Player {
     this.index = null;
     this.distance = 0;
     this.name = null;
+    this.rank = null;
   }
 
   getCount(){
@@ -25,11 +26,22 @@ class Player {
       distance:this.distance
     });
   }
-
   static getPlayerInfo(){
     var playerInfoRef = database.ref('players');
     playerInfoRef.on("value",(data)=>{
       allPlayers = data.val();
     })
   }
+  getCarsAtEnd(){
+    database.ref('carsAtEnd').on("value",(data)=>{
+      this.rank = data.val();
+    })
+  }
+  static updateCarsAtEnd(rank){
+    database.ref('/').update({
+      carsAtEnd:rank
+    })
+  }
 }
+
+
